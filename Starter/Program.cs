4f403bfbe5ac7b0e835c2e90805cc28bf7955455
uyanda-maseti <masetibabalo2@gmail.com>
2026-09-10 01:06:44 +0200

    Genesis 1:3

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,2 +1,3 @@
 Console.WriteLine("1. In the beginning, God created the heavens and the earth.");
 Console.WriteLine("2. The earth was without form and void, and darkness was over the face of the deep. And the spirit of God was hovering over the face of the waters.");
+Console.WriteLine("3. And God said, \"Let there be light,\" and there was light.");
